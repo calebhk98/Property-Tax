7e1050de2ab436c23a91c7cdaaf5b743303336a2
Caleb Kirschbaum <calebhk98@gmail.com>
2019-03-26 14:56:57 -0500

    Added Property Tax

---
+++ PropertyTax/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace PropertyTax
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.propertyValue = new System.Windows.Forms.TextBox();
+            this.errorLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.taxAmount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(438, 105);
+            this.label1.TabIndex = 0;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Please enter your property Value: ";
+            // 
+            // propertyValue
+            // 
+            this.propertyValue.Location = new System.Drawing.Point(214, 136);
+            this.propertyValue.Name = "propertyValue";
+            this.propertyValue.Size = new System.Drawing.Size(100, 20);
+            this.propertyValue.TabIndex = 2;
+            this.propertyValue.TextChanged += new System.EventHandler(this.propertyValue_TextChanged);
+            // 
+            // errorLabel
+            // 
+            this.errorLabel.AutoSize = true;
+            this.errorLabel.Location = new System.Drawing.Point(68, 205);
+            this.errorLabel.Name = "errorLabel";
+            this.errorLabel.Size = new System.Drawing.Size(262, 13);
+            this.errorLabel.TabIndex = 10;
+            this.errorLabel.Text = "Please make sure there is  only numbers in the texbox.";
+            this.errorLabel.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(125, 171);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Property Tax:";
+            // 
+            // taxAmount
+            // 
+            this.taxAmount.AutoSize = true;
+            this.taxAmount.Location = new System.Drawing.Point(214, 170);
+            this.taxAmount.Name = "taxAmount";
+            this.taxAmount.Size = new System.Drawing.Size(34, 13);
+            this.taxAmount.TabIndex = 13;
+            this.taxAmount.Text = "$0.00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 269);
+            this.Controls.Add(this.taxAmount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.errorLabel);
+            this.Controls.Add(this.propertyValue);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Property Tax";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox propertyValue;
+        private System.Windows.Forms.Label errorLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label taxAmount;
+    }
+}
+
---
+++ PropertyTax/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PropertyTax
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void propertyValue_TextChanged( object sender, EventArgs e)
+        {
+			//Vars
+            double tax = 0;
+            double propertyTax = 0;
+           if (double.TryParse(propertyValue.Text, out propertyTax))
+            {
+                errorLabel.Visible = false;//hides any error
+                tax = Math.Round(propertyTax /100 * 0.64, 2);//Math
+               
+
+                taxAmount.Text = "$" + tax.ToString();//Shows on screen
+                
+
+
+            }//Combines the try catch with parse
+            else if (propertyValue.Text == "")
+            {
+                errorLabel.Visible = false;
+                taxAmount.Text = "$0.00";
+
+            }//This hides any error, and only shows $0.00 if there is no value
+            else
+            {
+                errorLabel.Visible = true;
+            }//If something is wrong, wil show an error
+        }
+    }
+}
